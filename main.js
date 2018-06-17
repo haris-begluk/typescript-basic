d524cded51223d79ba30e1ce0edf86b3f1e245da
Haris Begluk <haris.begluk.webprogramming@outlook.com>
2018-06-17 12:26:25 +0200

    defining variable with let keyword

--- main.js
+++ main.js
@@ -1,4 +1,7 @@
 //Variable Declaration 
+//Kada deklarisemo varijablu sa var mozemo joj pristupiti bilo gdje, 
+//Dok kada deklarisemo varijablu sa let kljucnom rijeclju mozemo joj pristupiti, 
+//samo u bloku gdje je deklarisana.
 var number = 13;
 var count = 2;
 function doSomething() {
