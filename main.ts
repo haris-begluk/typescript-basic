89e7a99efa481bd431c35fec19a1c3c74f32a736
Haris Begluk <haris.begluk.webprogramming@outlook.com>
2018-06-17 12:39:23 +0200

    Type assertions

--- main.ts
+++ main.ts
@@ -1,18 +1,10 @@
-//Variable Declaration 
-let a; 
-a = 1; 
-a = 'Neko'; 
-a = true; 
-let b: number; 
-let c: boolean; 
-let d: string; 
-let e:any; 
-let f: number[] = [1,2,3]; 
-let g:any [] = [1,true,'Name', false]; 
+let message = 'abc'; 
+message.endsWith('c'); 
 
+let message2; 
+message2 = "abcd"; 
+let endsWithC = message2.endsWith('c'); 
 
-const ColorRed = 0; 
-const ColorGreen = 1; 
-const ColorBlue = 1; 
-enum Color {Red = 0, Green = 1 , Blue = 2 }; 
-let backgroundColor = Color.Red;
+//Type Assertions 
+let endsWithD = (<string>message).endsWith('d'); 
+let alternativeWay = (message as string).endsWith('d');
